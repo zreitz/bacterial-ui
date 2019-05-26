antismash/bacterial-ui-dev

start: Add a hmm detection strictness level control
Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

app/start/start.js
@@ -26,6 +26,15 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
                 vm.submission[feature.id] = feature.default;
             }
 
+            vm.strictness_levels = [
+                { id: 'strict', description: 'Detect well-defined clusters containing all required parts.' },
+                { id: 'relaxed', description: 'Detect partial clusters missing one or more functional parts.' },
+                { id: 'loose', description: 'Detect poorly-defined clusters and clusters that likely match primary metabolites.',
+                  warning: 'Likely to cause false positives.' },
+            ];
+
+            vm.hmmdetection_strictness = 1;
+
             vm.genefinder = 'prodigal';
 
             vm.submit = function (form) {
@@ -47,6 +56,8 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
                 if (vm.email) {
                     vm.submission.email = vm.email;
                 }
+
+                vm.submission.hmmdetection_strictness = vm.strictness_levels[vm.hmmdetection_strictness].id;
 
                 Upload.upload({
                     url: '/api/v1.0/submit',
@@ -167,5 +178,13 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
 
             vm.clearGff = function () {
                 vm.gff_file = null;
+            }
+
+            vm.strictness_descriptions = function () {
+                var descriptions = [];
+                for (var i = 0; i <= vm.hmmdetection_strictness; i++) {
+                    descriptions.push(vm.strictness_levels[i].description);
+                }
+                return descriptions;
             }
         }]);

zreitz/bacterial-ui

app/start/start.js
@@ -26,6 +26,15 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
                 vm.submission[feature.id] = feature.default;
             }
 
+            vm.strictness_levels = [
+                { id: 'strict', description: 'Detects well-defined clusters containing all required parts.' },
+                { id: 'relaxed', description: 'Detects partial clusters missing one or more functional parts.' },
+                { id: 'loose', description: 'Detects poorly-defined clusters and clusters that likely match primary metabolites.',
+                  warning: 'Likely to cause false positives.' },
+            ];
+
+            vm.hmmdetection_strictness = 1;
+
             vm.genefinder = 'prodigal';
 
             vm.submit = function (form) {
@@ -47,6 +56,8 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
                 if (vm.email) {
                     vm.submission.email = vm.email;
                 }
+
+                vm.submission.hmmdetection_strictness = vm.strictness_levels[vm.hmmdetection_strictness].id;
 
                 Upload.upload({
                     url: '/api/v1.0/submit',
@@ -164,5 +175,13 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
 
             vm.clearGff = function () {
                 vm.gff_file = null;
+            }
+
+            vm.strictness_descriptions = function () {
+                var descriptions = [];
+                for (var i = 0; i <= vm.hmmdetection_strictness; i++) {
+                    descriptions.push(vm.strictness_levels[i].description);
+                }
+                return descriptions;
             }
         }]);