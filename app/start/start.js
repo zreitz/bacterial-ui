60affddd49221afcd6c2feb1c03652cc63a505b5
Kai Blin <kblin@biosustain.dtu.dk>
2021-02-22 13:38:09 +0100

    start: Allow uploading sideloaded results
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- app/start/start.js
+++ app/start/start.js
@@ -10,6 +10,8 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
 
             vm.run_beta = true;
 
+            vm.upload_sideload_file = false;
+
             // Defaullt values
             vm.submission = {};
 
@@ -56,6 +58,10 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
                     }
                 } else {
                     vm.submission.ncbi = vm.ncbi;
+                }
+
+                if (vm.run_beta && vm.sideload_file) {
+                    vm.submission.sideload = vm.sideload_file;
                 }
 
                 if (vm.email) {
